c275304d4caff6ed7b323ba8b8a25bbf1afb44b0
Kenji <fcb141021@yahoo.co.jp>
2023-08-25 21:43:37 +0900

    propertysコントローラの作成

---
+++ app/controllers/propertys_controller.rb
@@ -0,0 +1,10 @@
+class PropertysController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/propertys_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PropertysControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get propertys_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get propertys_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get propertys_edit_url
+    assert_response :success
+  end
+
+end
